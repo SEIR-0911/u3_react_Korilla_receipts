6183055fc8f62a27fbdeecd8910cf0e562f3c427
MichaelFami <michaelfami@gmail.com>
2023-10-26 22:30:25 -0500

    first blood

---
+++ korilla/src/App.jsx
@@ -0,0 +1,14 @@
+import Receipt from './components/Receipt'
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+        <Receipt/>
+      </div>
+      
+  )
+}
+
+export default App
